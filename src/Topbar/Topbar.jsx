b0a1acf816fa6653e73c4f2b07eb9eaba360ab0a
Lucas Silva <martssilvapro@gmail.com>
2021-08-11 11:22:10 -0300

    configura elementos do topbar

--- src/Topbar/Topbar.jsx
+++ src/Topbar/Topbar.jsx
@@ -3,10 +3,29 @@ import './topbar.css';
 
 const Topbar = () => {
   return (
-    <div className='top'>
-      <div className='topLeft'>l</div>
-      <div className='topCenter'>c</div>
-      <div className='topRight'>r</div>
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab fa-facebook-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-instagram-square"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">WRITE</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img
+        className="topImg"
+        src="https://lh3.googleusercontent.com/ogw/ADea4I4RRWSilQr_8R8GGiLEU5dITRp4qdWmCL5s4TJPPw=s83-c-mo"
+        alt="" />
+        <i className="topSearchIcon fas fa-search"></i>
+      </div>
     </div>
   )
 }

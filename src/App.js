e7371b8d9c623acdb9f26988631b9676127bb7f2
Lucas Silva <martssilvapro@gmail.com>
2021-08-13 11:26:40 -0300

    implementa formulario de adiçao de conteudo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 // import Home from "./Pages/Home/Home";
 import Topbar from "./components/Topbar/Topbar.jsx";
-import Single from "./Pages/Single/Single";
+// import Single from "./Pages/Single/Single";
+import Write from "./Pages/Write/Write.jsx";
 
 
 function App() {
   return (
     <>
       <Topbar />
-      <Single />
+      <Write />
     </>
   );
 }
--- src/Pages/Write/Write.jsx
+++ src/Pages/Write/Write.jsx
@@ -2,10 +2,26 @@ import './write.css';
 
 const Write = () => {
   return (
-    <div>
-
-    </div>
-  )
+    <section className="write">
+      <form className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+            <i class="fas fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{ display: "none" }}/>
+          <input type="text" placeholder="title" className="writeInput" autoFocus={true} />
+        </div>
+        <div className="writeFormGroup">
+          <textarea
+            placeholder="Tell your story..."
+            type="text"
+            className="writeInput writeText">
+          </textarea>
+        </div>
+        <button className="writeSubmit">Publish</button>
+      </form>
+    </section>
+  );
 }
 
 export default Write;

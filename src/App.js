bdc7f09a989faa9f5de11cfbe8e5c8ad98999df7
Lucas Silva <martssilvapro@gmail.com>
2021-08-18 11:16:45 -0300

    adiciona e implementa react-router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-// import Home from "./Pages/Home/Home";
+import Home from "./Pages/Home/Home";
 import Topbar from "./components/Topbar/Topbar.jsx";
-// import Login from "./Pages/Login/Login.jsx";
+import Login from "./Pages/Login/Login.jsx";
 import Register from "./Pages/Register/Register.jsx";
-// import Settings from "./Pages/Settings/Settings.jsx";
-// import Single from "./Pages/Single/Single";
-// import Write from "./Pages/Write/Write.jsx";
+import Write from "./Pages/Write/Write.jsx";
+import Settings from "./Pages/Settings/Settings.jsx";
+import Single from "./Pages/Single/Single";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Topbar />
-      <Register />
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/write">
+          <Write />
+        </Route>
+        <Route path="/settings">
+          <Settings />
+        </Route>
+        <Route path="/post/:postId">
+          <Single />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 

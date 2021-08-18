c9b4538cf9594f56e2a8d48d44827c94c54e49d6
Lucas Silva <martssilvapro@gmail.com>
2021-08-18 10:35:06 -0300

    página de login estilizada

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,11 +3,12 @@ import './login.css';
 const Login = () => {
   return (
     <section className="login">
+    <span className="loginTitle">Welcome</span>
       <form className="loginForm">
         <label >Email</label>
-        <input type="text" placeholder="Enter your email..." />
+        <input type="text" className="loginInput" placeholder="Enter your email..." />
         <label >Password</label>
-        <input type="text" placeholder="Enter your password..." />
+        <input type="text" className="loginInput" placeholder="Enter your password..." />
         <button className="loginButton">Login</button>
       </form>
       <button className="loginRegisterButton">Register</button>

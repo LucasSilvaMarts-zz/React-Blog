bb821a563167899d14359846ddf534d71c2cee38
Lucas Silva <martssilvapro@gmail.com>
2021-08-18 10:36:31 -0300

    adiciona página de registro

---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,19 @@
+import './register.css';
+
+const Register = () => {
+  return (
+    <section className="login">
+    <span className="loginTitle">Welcome</span>
+      <form className="loginForm">
+        <label >Email</label>
+        <input type="text" className="loginInput" placeholder="Enter your email..." />
+        <label >Password</label>
+        <input type="text" className="loginInput" placeholder="Enter your password..." />
+        <button className="loginButton">Login</button>
+      </form>
+      <button className="loginRegisterButton">Register</button>
+    </section>
+  );
+}
+
+export default Register;

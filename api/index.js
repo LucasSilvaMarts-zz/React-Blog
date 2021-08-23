47b8f19ca48e80f150d5a4dbba1e9ba46d7ad34c
Lucas Silva <martssilvapro@gmail.com>
2021-08-23 15:40:14 -0300

    configura conexão com mongo

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const port = 3001;
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
+  .then(console.log('connected'))
+  .catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`App listening on port ${port}!`));

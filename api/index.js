b8d21d4bd3a72add58020bf05d71e206132ea7cc
Lucas Silva <martssilvapro@gmail.com>
2021-08-24 16:35:43 -0300

    implementa multer

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const multer = require('multer');
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
@@ -18,6 +19,21 @@ mongoose.connect(process.env.MONGO_URL, {
 })
   .then(console.log('connected'))
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name)
+  },
+});
+
+const upload = multer({ storage });
+
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
+})
 
 app.use('/api/auth', authRoute);
 

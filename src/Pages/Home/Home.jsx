bc16eb0fd6967d9bc1be02062fbe63489b738662
Lucas Silva <martssilvapro@gmail.com>
2021-08-25 10:53:07 -0300

    implementa função de fetch dos dados do db

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,21 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import Header from '../../components/Header/Header';
 import Posts from '../../components/Posts/Posts';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import './home.css'
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+   const fetchPosts = async () => {
+    const res = await axios.get('/posts');
+    console.log(res)
+   }
+   fetchPosts();
+  }, [])
+
   return (
     <>
     <Header />
